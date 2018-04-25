c4103920ba8d363cf405ef2d387c3f088f934b24
Evan Yeh <eyeh2809@yahoo.com>
2018-04-25 09:03:34 -0700

    Update leading_ones - working, needs some comments

--- hw2/leading_ones/simulation/qsim/work/leading_ones/_primary.vhd
+++ hw2/leading_ones/simulation/qsim/work/leading_ones/_primary.vhd
@@ -3,6 +3,7 @@ use verilog.vl_types.all;
 entity leading_ones is
     port(
         x               : in     vl_logic_vector(7 downto 0);
+        ssd             : out    vl_logic_vector(6 downto 0);
         y               : out    vl_logic_vector(31 downto 0)
     );
 end leading_ones;
--- hw2/leading_ones/simulation/qsim/work/leading_ones_vlg_check_tst/_primary.vhd
+++ hw2/leading_ones/simulation/qsim/work/leading_ones_vlg_check_tst/_primary.vhd
@@ -2,6 +2,7 @@ library verilog;
 use verilog.vl_types.all;
 entity leading_ones_vlg_check_tst is
     port(
+        ssd             : in     vl_logic_vector(6 downto 0);
         y               : in     vl_logic_vector(31 downto 0);
         sampler_rx      : in     vl_logic
     );
