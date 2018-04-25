b314fd690b6a636e85f417a5c38f91d7c946b5da
Evan Yeh <eyeh2809@yahoo.com>
2018-04-24 21:52:51 -0700

    Update absolute difference

--- hw2/absolute_difference/absolute_difference.vhd
+++ hw2/absolute_difference/absolute_difference.vhd
@@ -6,6 +6,7 @@ use ieee.math_real.all;
 use work.user_types.all;
 --------------------------------------------------------------------------------
 entity absolute_difference is
+	--integer(ceil(log2(SIZE+1)))
 	generic (
 		EXTRABITS: natural := 0;
 		SIZE: natural := 6;
@@ -17,11 +18,22 @@ end entity;
 --------------------------------------------------------------------------------
 architecture generic_chain of absolute_difference is
 	type signed_array is array (natural range <>) of signed;
-	signal a_sig, b_sig: signed_array(0 to SIZE - 1)(BITS + EXTRABITS - 1 downto 0);
-	signal abs_diff_sig: std_logic_vector(BITS + EXTRABITS - 1 downto 0);
+	type signed_array is array (natural range <>) of signed;
+	signal abs_diffs: signed_array(0 to SIZE)(BITS + EXTRABITS - 1 downto 0);
+	signal sums: signed_array(0 to SIZE);
 begin
 	-- have a_sig and b_sig that are arrays of signed numbers
 	-- loop through all SIZE elements and find difference from abs(signed(element) - signed(element))
 	-- sum all differences and store in abs_diff using `std_logic(signed_sum)` signed_sum is the result from above
 	-- actually might not need the a_sig and b_sig if doing it all element-wise
+
+	-- puts absolute differences into a signed array
+	sums(0) <= abs(signed(resize(a(0), BITS + EXTRABITS)) - signed(resize(b(0), BITS + EXTRABITS)));
+	gen: for i in 1 to SIZE - 1 generate
+		abs_diffs(i) <= abs(signed(resize(a(i), BITS + EXTRABITS)) - signed(resize(b(i), BITS + EXTRABITS)));
+		sums(i) <= sums(i-1) + abs_diffs(i);
+	end generate;
+
+
+
 end architecture;
