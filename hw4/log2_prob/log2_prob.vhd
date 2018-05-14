7d50feabbdac45011ad1a150ebbe658489cc2945
Evan Yeh <eyeh2809@yahoo.com>
2018-05-14 00:07:31 -0700

    ceil log2 function done

--- hw4/log2_prob/log2_prob.vhd
+++ hw4/log2_prob/log2_prob.vhd
@@ -7,15 +7,16 @@ use ieee.math_real.all;
 entity log2_prob is
 	generic (BITS: natural := 8);
 	port (x: in std_logic_vector(BITS - 1  downto 0);
-			--y: out std_logic_vector(ceil(log2(BITS)) downto 0); not synthesizeable
+			-- generically:
+			-- y: out std_logic_vector(ceil(log2(BITS)) downto 0); not synthesizeable
+			-- for simulation:
 			y: out std_logic_vector(3 downto 0));
 end entity;
 -------------------------------------------------------------------------
 architecture problem_one of log2_prob is
 	function ceil_log2 (input: positive) return integer is
-		variable i: integer := 1;
+		variable i: integer := 0;
 	begin
-		i := 0;
 		while (2 ** i < input and i <= BITS) loop
 			i := i + 1;
 		end loop;
@@ -25,7 +26,9 @@ architecture problem_one of log2_prob is
 	signal x_int: integer;
 begin
 	x_int <= positive(to_integer(unsigned(x)));
-	--y <= std_logic_vector(to_unsigned(ceil_log2(x_int), ceil_log2(BITS));
+	-- generically:
+	-- y <= std_logic_vector(to_unsigned(ceil_log2(x_int), ceil_log2(BITS));
+	-- for simulation:
 	y <= std_logic_vector(to_unsigned(ceil_log2(x_int), 4));
 end architecture;
 -------------------------------------------------------------------------
