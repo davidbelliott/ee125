dd7be72645d012be5c30c962224ee5876f32d646
Evan Yeh <eyeh2809@yahoo.com>
2018-06-04 16:20:57 -0700

    Add buffer type to package

--- linewars/linewars.vhd
+++ linewars/linewars.vhd
@@ -2,16 +2,14 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 ----------------------------------------------------------
 ENTITY linewars IS
-	GENERIC (
-		BOARD_W: INTEGER := 80;
-		BOARD_H: INTEGER := 60;
+	--GENERIC ();
 	PORT (
 		clk: IN STD_LOGIC; --50MHz in our board
 		p1lswitch, p1rswitch, p2lswitch, p2rswitch: IN STD_LOGIC;
 		Hsync, Vsync: BUFFER STD_LOGIC;
 		R, G, B: OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
 END linewars;
-type t_buffer is array (0 to BOARD_H - 1) of std_logic_vector(0 to BOARD_W - 1);
+
 ----------------------------------------------------------
 ARCHITECTURE linewars OF linewars IS
 	SIGNAL display_buffer: t_buffer;
